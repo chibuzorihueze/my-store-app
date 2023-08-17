f6936d2634e71ad15df73dbf972908677d39e2a5
chibuzor ihueze <krisykord@gmail.com>
2023-08-17 11:21:35 +0100

    mantine theme config

---
+++ src/config/theme.config.js
@@ -0,0 +1,4 @@
+export const MantineTheme = {
+  fontFamily: "Open Sans, sans serif",
+  colorScheme: "light",
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { Button, MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { MantineTheme } from "./config/theme.config";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        fontFamily: "Open Sans, sans serif",
+        colorScheme: "light",
+        loader: "bars",
+      }}
+    >
       <Button>test</Button>
     </MantineProvider>
   </React.StrictMode>
