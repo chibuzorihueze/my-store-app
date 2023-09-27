af4bd2bc4f05ec81145740df938571a3850a4cdc
chibuzor ihueze <krisykord@gmail.com>
2023-09-27 10:39:51 +0100

    lint changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        index: "about",
+        path: "about",
         element: <About />,
       },
     ],
